b2aa8ce50c87ea5a3f6c7b58f761a1732ab39050
jiruffe <konarchen@live.com>
2022-03-18 09:26:06 +0000

    Update at 2022-03-18T09:26:06.163305Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,14 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int a = 0, b = 0;
+        if (null != a) {
+            a = Integer.parseInt(a);
+        }
+        if (null != b) {
+            b = Integer.parseInt(b);
+        }
+        return a + b + "";
+    }
 }
