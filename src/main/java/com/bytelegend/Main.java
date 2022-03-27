ef99c56d5edba6711036e77f4cecabd3f97a24a7
daydreaming666 <indigodream@foxmail.com>
2022-03-27 10:49:41 +0000

    Update at 2022-03-27T10:49:41.064314Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,9 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int v1 = Objects.isNull(a) ? 0 : Integer.parseInt(a);
+        int v2 = Objects.isNull(b) ? 0 : Integer.parseInt(b);
+        return Integer.toString(v1 + v2);
+    }
 }
