c1bc88e56a30c803f20c931f1d50b688994ef81d
denghaoze <57385221+denghaoze@users.noreply.github.com>
2022-04-04 08:23:52 +0000

    Update at 2022-04-04T08:23:52.114782Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,11 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        Intger a1 = null;
+        Intger b1 = null;
+        if (a == null) a1 = 0;
+        if (b == null) b1 = 0;
+        return (a1 + b1).toString();
+    }
 }
