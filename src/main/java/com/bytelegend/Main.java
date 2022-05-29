ceeaa6537174d59f4fb1655295caa812dbf81775
andrewbaxter439 <andy@andybaxter.me>
2022-05-29 22:45:15 +0000

    Update at 2022-05-29T22:45:15.577271Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,29 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+
+        int aI;
+        int bI;
+
+        try
+        {
+            aI = Integer.parseInt(a);
+        }
+        catch (NumberFormatException e)
+        {
+            aI = 0;
+        }
+
+        try
+        {
+            bI = Integer.parseInt(b);
+        }
+        catch (NumberFormatException e)
+        {
+            bI = 0;
+        }
+        String result = String.valueOf(aI + bI);
+        return result;
+    }
 }
