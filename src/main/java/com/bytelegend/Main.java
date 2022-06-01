5ee5e06cbe8064f9cb2f577ff86cf48b8f4fac09
s9x <rich@xtinction.org>
2022-06-01 01:22:31 +0000

    Update at 2022-06-01T01:22:31.215991Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,15 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        if (a == null) {
+            a = "0";
+        }
+
+        if (b == null) {
+            b = "0";
+        }
+
+        return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
+    }
 }
