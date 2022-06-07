f4d6b9e5b8be9b01833a4e14eb8927ffcb3a6e15
jursjohn <johnjurassic93@yahoo.com.ph>
2022-06-07 03:37:28 +0000

    Update at 2022-06-07T03:37:28.553362Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,15 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int aInt = 0;
+        int bInt = 0;
+        if (a != null) {
+            aInt = Integer.valueOf(a);
+        }
+        if (b != null) {
+            bInt = Integer.valueOf(b);
+        }
+        return String.valueOf(aInt + bInt);
+    }
 }
