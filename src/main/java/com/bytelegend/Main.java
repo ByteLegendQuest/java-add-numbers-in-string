c8759dcf44cd3a82a98bc0fb727f1194ae616d5e
jursjohn <johnjurassic93@yahoo.com.ph>
2022-06-07 03:12:33 +0000

    Update at 2022-06-07T03:12:33.922884Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,21 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        if (checkForNull(a)) {
+            a = "0";
+        }
+        if (checkForNull(b)) {
+            b = "0";
+        }
+        int addedValue = Integer.valueOf(a) + Integer.ValueOf(b);
+        return String.valueOf(addedValue);
+    }
+
+    public static boolean checkForNull(String a) {
+        if (a == null) {
+            return true;
+        }
+        return false;
+    }
 }
