fd3322543e3234b668c3773c85bc71fbac7ec10f
SpaceTimeAdministrator <106767177+SpaceTimeAdministrator@users.noreply.github.com>
2022-06-11 14:37:36 +0000

    Update at 2022-06-11T14:37:36.716004Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,15 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+             int c= 0;
+        int d = 0;
+        if (a != null) {
+            c = Integer.valueOf(a);
+        }
+        if (b != null) {
+            d = Integer.valueOf(b);
+        }
+        return String.valueOf(c + d);
+    }
 }
