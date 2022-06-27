c539bc311cafa1ec988d456b0cd97bcad82f7e52
Anlyui <1391469813@qq.com>
2022-06-27 12:46:09 +0000

    Update at 2022-06-27T12:46:09.320772Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,13 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        if (a == null) {
+            return 0 + b;
+            if (b == null) {
+                return 0;
+            }
+        }
+        return a + b;
+    }
 }
