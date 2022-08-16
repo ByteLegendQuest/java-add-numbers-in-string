3986ff535d3f0a550b919161655e5593dbf178f8
lpkyyds <2065870265@qq.com>
2022-08-16 00:20:28 +0000

    Update at 2022-08-16T00:20:28.754614Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,15 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int intA = 0;
+        int intB = 0;
+        if (null == a) {
+            intA = Integer.parseInt(a);
+        }
+        if (null == b) {
+            intB = Integer.parseInt(b);
+        }
+        return String.valueOf(intA + intB);
+    }
 }
