ac7baa46c6d29b45c9b2de88a2be6f0c4be52d30
slsy0 <1485807135@qq.com>
2022-09-02 07:46:12 +0000

    Update at 2022-09-02T07:46:12.621901Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,24 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        if (a == null) {
+            int a1 = 0;
+            int b1 = Integar.valueOf(b);
+            return a1 + b1;
+        }
+        else if (b == null) {
+            int b1 = 0;
+            int a1 = Integar.valueOf(a);
+            return a1 + b1;
+        }
+        else if (a == null && b == null) {
+            return 0;
+        }
+        else {
+            int a1 = Integar.valueOf(a);
+            int b1 = Integar.valueOf(b);
+            return a1 + b1;
+        }
+    }
 }
