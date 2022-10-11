884e6806ba7e13705e8dcdea42ce8b29e502ecf8
pipiloveslife <Pain_Zzz@163.com>
2022-10-11 07:01:26 +0000

    Update at 2022-10-11T07:01:26.230770Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(add("123", "456"));
@@ -7,5 +9,9 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int numberA = Integer.parseInt(Optional.ofNullable(a).orElse("0"));
+        int numberB = Integer.parseInt(Optional.ofNullable(b).orElse("0"));
+        return "" + (numberA + numberB);
+    }
 }
