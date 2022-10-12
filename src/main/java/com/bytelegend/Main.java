057a777a64be11d8366a0fe112afc40faf79a5f9
tiger0961 <29515547@qq.com>
2022-10-12 06:36:16 +0000

    Update at 2022-10-12T06:36:16.988116Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,10 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+       int n = a != null ? Integer.parseInt(a) : 0;
+       int m = b != null ? Integer.parseInt(b) : 0;
+
+       return (a + b).toString();
+    }
 }
