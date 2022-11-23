978befc83833be06e0003aa91e7e2723d1b67ae5
williamgl <ligan@oregonstate.edu>
2022-11-23 19:55:08 +0000

    Update at 2022-11-23T19:55:08.168161Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,18 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int numA, numbB;
+        if (a == null) {
+            numA = 0;
+        } else {
+            numA = Integer.parseInt(a);
+        }
+        if (b == null) {
+            numbB = 0;
+        } else {
+            numB = Integer.parseInt(b);
+        }
+        return String.valueof(numA + numbB);
+    }
 }
