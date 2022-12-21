bf7fd6b1957fbaf8590052811d78184a66396524
Qiqi7-7 <writetomars@qq.com>
2022-12-21 04:23:46 +0000

    Update at 2022-12-21T04:23:46.224152Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,14 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int aa = 0, bb = 0;
+        if (a) {
+            aa = Integer.parseInt(a);
+        }
+        if (b) {
+            bb = Integer.parseInt(b);
+        }
+        return String.valueOf(aa + bb);
+    }
 }
