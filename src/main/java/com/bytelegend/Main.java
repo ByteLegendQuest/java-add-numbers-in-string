e9c63bfe6f0cf3bce3583a9439799adb56985927
AaronFledermaus <78683536+aaronfledermaus@users.noreply.github.com>
2023-02-11 11:53:22 +0800

    Update Main.java

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,17 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int a1, b1;
+        if(a==null)
+            a1=0;
+        else
+            a1=Integer.parseInt(a);
+        if(b==null)
+            b1=0;
+        else
+            b1=Integer.parseInt(b);
+        return String.valueOf(a1+b1);
+        
+    }
 }
