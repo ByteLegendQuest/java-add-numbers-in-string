adf29697d333b41b6732f464df5a5ad50fe27bc1
BasilGuo <basilguo@163.com>
2023-02-21 02:11:08 +0000

    Update at 2023-02-21T02:11:08.306477Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,15 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        if (a == null && b == null) {
+            return "0";
+        } else if (a == null) {
+            return b;
+        } else if (b == null) {
+            return a;
+        }
+
+        return String.valueOf(Integer.parseInt(a) + Integer.parseInt(b));
+    }
 }
