3162924b1802d1dcb276d499a54bb8c67733e56f
guakun01 <guakun01@gmail.com>
2023-03-18 13:36:15 +0000

    Update at 2023-03-18T13:36:15.305314Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,7 +6,6 @@ public class Main {
         System.out.println(add("123", null));
         System.out.println(add(null, null));
     }
-
     public static String add(String a, String b) {
         int c = 0;
         int d = 0;
