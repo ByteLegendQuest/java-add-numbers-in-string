e6356bf06696bcdc97cf9cea62282f6ab79fa29c
DasBrain <johannes13@johannes13.de>
2023-05-20 15:36:09 +0000

    Update at 2023-05-20T15:36:09.771589Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -12,8 +12,8 @@ public class Main {
         if (a == null) a = "0";
         if (b == null) b = "0";
         for (int i = 0; i < a.length() || i < b.length(); i++) {
-            int v1 = a != null && i < a.length() : Integer.parseInt(a.charAt(i));
-            int v2 = b != null && i < b.length() : Integer.parseInt(b.charAt(i));
+            int v1 = i < a.length() ? Integer.parseInt(a.charAt(i)) : 0;
+            int v2 = i < b.length() ? Integer.parseInt(b.charAt(i)) : 0;
             result += v1 + v2;
         }
         return result;
