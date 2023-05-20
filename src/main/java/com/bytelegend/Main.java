ac3d26fa894fd90fd4a5bd2bbcb1df1ccc081beb
DasBrain <johannes13@johannes13.de>
2023-05-20 15:33:05 +0000

    Update at 2023-05-20T15:33:05.414785Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     public static String add(String a, String b) {
         String result = "";
+        if (a == null) a = "0";
+        if (b == null) b = "0";
         for (int i = 0; i < a.length() || i < b.length(); i++) {
             int v1 = a != null && i < a.length() : Integer.parseInt(a.charAt(i));
             int v2 = b != null && i < b.length() : Integer.parseInt(b.charAt(i));
