539e1aec4f42354180840c787928b2136f5d1776
Jungmin-Moon <jmmoon96@gmail.com>
2023-05-30 04:16:18 +0000

    Update at 2023-05-30T04:16:18.114728Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,23 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        int sum = 0;
+        if(a == null) {
+            if(b == null) {
+                return 0;
+            } else {
+                return b;
+            }
+        } else if(b == null) {
+            if(a == null) {
+                return 0;
+            } else {
+                return a;
+            }
+            
+        } else {
+            return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
+        }
+    }
 }
