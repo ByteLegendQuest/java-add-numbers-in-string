1f87232c444f076db13997c42475451275e69625
HumansDoNotWantImmortality <rosinski.krzysztof.01@gmail.com>
2023-06-01 16:37:14 +0000

    Update at 2023-06-01T16:37:14.085137Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println(add(null, null));
     }
 
-    public static String add(String a, String b) {}
+    public static String add(String a, String b) {
+        return "" + ((a == null ? 0 : Integer.parseInt(a)) + (b == null ? 0 : Integer.parseInt(b)));
+    }
 }
